eeab3f6dfc6a6b4c1801153969597c171d41876e
Adam Brodzinski <adambrodzinski@gmail.com>
2015-10-24 19:14:18 -0400

    adds support for parens and cleans up

--- test/examples/pipe_operator.js
+++ test/examples/pipe_operator.js
@@ -1,12 +1,18 @@
 var foo = 2
 
-var res = _.chain(foo)
-.pipesCall(add, 10, 20).value();
+foo
+.pipesCall(add, 10, 20)
+.pipesCall(has_parens, 10, 20)
+.pipesCall(noparens)
+.pipesCall(emprty_parens).value();
 //|> square 2
 //|> map (x) => x * 2
 //|> square
 
 console.log(res);
+
+console.log("foo |> par");
+console.log('foo |> par');
 
 // |> square 2
 // .thru(square, 2)
--- test/examples/pipe_operator.rs
+++ test/examples/pipe_operator.rs
@@ -1,12 +1,18 @@
 foo = 2
 
-res = _.chain(foo)
+foo
 |> add 10, 20
+|> has_parens(10, 20)
+|> noparens
+|> emprty_parens()
 //|> square 2
 //|> map (x) => x * 2
 //|> square
 
 puts res
+
+puts "foo |> par"
+puts 'foo |> par'
 
 # |> square 2
 # .thru(square, 2)
