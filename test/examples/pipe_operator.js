015e781baea4c95c0bf7eb722b13d9006060b581
Adam Brodzinski <adambrodzinski@gmail.com>
2015-10-24 21:10:08 -0400

    fixes lambda error

--- test/examples/pipe_operator.js
+++ test/examples/pipe_operator.js
@@ -1,37 +1,10 @@
-var foo = 2
 
-var res = _.chain(foo)
-.pipesCall(add, 10, 20)
-.pipesCall(has_parens, 10, 20)
-.pipesCall(noparens)
-.pipesCall(emprty_parens).value();
+foo = [1, 2, 3] |> Enum.uppcase
 
 _.chain(res)
-.pipesCall(add, 10, 20)
 .pipesCall(has_parens, 10, 20)
-.pipesCall(map, x) => x * 2).value();
+.pipesCall(map, (x) => x * 2)
+.pipesCall(map, ((x) => x * 2))
+.pipesCall(map, (x => x * 2))
+.pipesCall(map, x => x * 2).value();
 
-.pipesCall(emprty_parens).value();
-
-
-return _.chain({foo: 1, bar: 2})
-.pipesCall(add, 10, 20).value();
-
-_.chain([1, 2, 22])
-.pipesCall(add, 10, 20).value();
-
-//|> square 2
-//|> map (x) => x * 2
-//|> square
-
-console.log(res);
-
-console.log("foo |> par");
-console.log('foo |> par');
-
-// |> square 2
-// .thru(square, 2)
-
-function square(x) {
-  return x * x;
-}
--- test/examples/pipe_operator.rs
+++ test/examples/pipe_operator.rs
@@ -1,37 +1,10 @@
-foo = 2
 
-res = foo
-|> add 10, 20
-|> has_parens(10, 20)
-|> noparens
-|> emprty_parens()
+foo = [1, 2, 3] |> Enum.uppcase
 
 res
-|> add 10, 20
 |> has_parens(10, 20)
 |> map (x) => x * 2
+|> map((x) => x * 2)
+|> map(x => x * 2)
+|> map x => x * 2
 
-|> emprty_parens()
-
-
-return {foo: 1, bar: 2}
-|> add 10, 20
-
-[1, 2, 22]
-|> add 10, 20
-
-//|> square 2
-//|> map (x) => x * 2
-//|> square
-
-puts res
-
-puts "foo |> par"
-puts 'foo |> par'
-
-# |> square 2
-# .thru(square, 2)
-
-function square(x) {
-  return x * x;
-}
