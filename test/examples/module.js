ed7b8d97f0f33ab29cb3918ac4758c067243799c
Adam Brodzinski <adambrodzinski@gmail.com>
2015-10-25 20:10:53 -0400

    adds define module transformation
    basically removes module def and leaves comment, strips last `end`

---
+++ test/examples/module.js
@@ -0,0 +1,13 @@
+// Module: Calculator
+  export function add(a, b) {
+    return a + b
+  }
+
+  export function multiply(a, b) {
+    return a * b
+  }
+
+  function say_hi() {
+    console.log("hello")
+  }
+
---
+++ test/examples/module.rs
@@ -0,0 +1,13 @@
+defmodule Calculator do
+  def add(a, b) do
+    return a + b
+  end
+
+  def multiply(a, b) do
+    return a * b
+  end
+
+  defp say_hi do
+    console.log("hello")
+  end
+end
